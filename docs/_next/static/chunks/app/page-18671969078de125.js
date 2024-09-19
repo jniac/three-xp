01dd80fee351ca65371484fd0555547cf8129707
jniac <joseph.merdrignac@gmail.com>
2024-09-19 02:34:56 +0200

    deploy (docs)

--- docs/_next/static/chunks/app/page-18671969078de125.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,698],{82631:function(n,e,u){Promise.resolve().then(u.t.bind(u,31390,23))}},function(n){n.O(0,[390,692,524,744],function(){return n(n.s=82631)}),_N_E=n.O()}]);
