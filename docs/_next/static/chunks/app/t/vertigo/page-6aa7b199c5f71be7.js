5a55182f0867407be2bc9a5e9839abf05e9721ce
jniac <joseph.merdrignac@gmail.com>
2024-11-08 15:40:53 +0100

    deploy (docs)

---
+++ docs/_next/static/chunks/app/t/vertigo/page-6aa7b199c5f71be7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6770],{25442:function(n,e,u){Promise.resolve().then(u.bind(u,84268))}},function(n){n.O(0,[5550,5244,6666,1729,4940,8648,4831,494,9088,2920,7936,1744],function(){return n(n.s=25442)}),_N_E=n.O()}]);
