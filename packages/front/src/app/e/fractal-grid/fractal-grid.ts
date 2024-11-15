d9094423a74b66d1c93628041393f56f44c06592
jniac <joseph.merdrignac@gmail.com>
2024-11-15 16:48:32 +0100

    fractal-grid

--- packages/front/src/app/e/fractal-grid/fractal-grid.ts
+++ packages/front/src/app/e/fractal-grid/fractal-grid.ts
@@ -1,10 +1,14 @@
-import { ColorRepresentation, Mesh, PlaneGeometry, TorusKnotGeometry } from 'three'
+import { ColorRepresentation, Mesh, MeshBasicMaterial, PlaneGeometry, TorusKnotGeometry, Vector3 } from 'three'
 import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
+import { Chunk, createNaiveVoxelGeometry } from 'some-utils-three/experimental/voxel'
+import { AxesGeometry } from 'some-utils-three/geometries/axis'
 import { AutoLitMaterial } from 'some-utils-three/materials/auto-lit'
 import { setvertexColors } from 'some-utils-three/utils/geometry'
 import { setup } from 'some-utils-three/utils/tree'
+import { loop2, loop3 } from 'some-utils-ts/iteration/loop'
 
+import { SkyMesh } from 'some-utils-three/objects/sky-mesh'
 import { useGroup } from './three-provider'
 
 const blockSize = 4
@@ -49,14 +53,92 @@ class BasicGrid extends Mesh {
   }
 }
 
+const BLOCK_SIZE = 4
+
+class VoxelGrid extends Mesh {
+  constructor({
+    scale = 1,
+    col = 4,
+    row = 4,
+    color = <ColorRepresentation>'#fff',
+  } = {}) {
+    const chunk = new Chunk(48, 1)
+
+    const p = new Vector3()
+    const q = new Vector3()
+
+    const cube = (p: Vector3, size: number) => {
+      for (const v of loop3(size, size, size)) {
+        const x = p.x + v.x
+        const y = p.y + v.y
+        const z = p.z + v.z
+        chunk.getVoxelState(x, y, z).setInt8(0, 1)
+      }
+    }
+
+    for (const { x, y } of loop2(col, row)) {
+      p.set(x, y, x + col - y)
+      q.x = p.x * BLOCK_SIZE
+      q.y = p.y * BLOCK_SIZE + BLOCK_SIZE / 2
+      q.z = p.z * BLOCK_SIZE
+      cube(q, BLOCK_SIZE)
+    }
+
+    for (let x = 0; x < col; x++) {
+      p.set(x, 0, x + col)
+      q.x = p.x * BLOCK_SIZE
+      q.y = p.y * BLOCK_SIZE
+      q.z = (p.z + 1) * BLOCK_SIZE
+      cube(q, 2)
+      q.x += 2
+      cube(q, 2)
+      q.z += 2
+      cube(q, 2)
+    }
+
+    const scaleScalar = scale / 2 / BLOCK_SIZE
+    const geometry = createNaiveVoxelGeometry(chunk.voxelFaces())
+      .scale(scaleScalar, scaleScalar, scaleScalar)
+
+    const material = new AutoLitMaterial({ color: '#fff' })
+
+    super(geometry, material)
+
+    setvertexColors(this.geometry, color)
+  }
+}
+
 export function FractalGrid() {
   useGroup('fractal-grid', function* (group) {
     setup(new Mesh(
       new TorusKnotGeometry(2.5, .025, 512, 32),
-      new AutoLitMaterial({ color: '#0cf' })), group)
+      new AutoLitMaterial({ color: '#0cf' })), group).visible = false
 
-    setup(new BasicGrid(), group)
-    setup(new BasicGrid({ color: 'red' }), { parent: group, x: blockSize * 3 * Math.SQRT2 })
+    // setup(new BasicGrid(), group)
+    // setup(new BasicGrid({ color: 'red' }), { parent: group, x: blockSize * 3 * Math.SQRT2 })
+    const p1 = setup(new VoxelGrid({}), group)
+    const p2 = setup(new VoxelGrid({
+      scale: .5,
+      color: '#f5e532'
+    }), {
+      parent: group,
+      y: -(BLOCK_SIZE + .5) / 4,
+      z: (BLOCK_SIZE + 1) / 2,
+    })
+    const p3 = setup(new VoxelGrid({
+      scale: .25,
+      color: '#3532f5'
+    }), {
+      parent: group,
+      y: -(BLOCK_SIZE + .5) / 4 - (BLOCK_SIZE + .5) / 4 / 2,
+      z: (BLOCK_SIZE + 1) / 2 + (BLOCK_SIZE + 1) / 2 / 2,
+    })
+
+    setvertexColors(p1.geometry, '#f5327d', 0, 10560 / 4)
+
+    setup(new SkyMesh({ color: '#110512' }), group)
+
+    setup(new Mesh(new AxesGeometry(), new MeshBasicMaterial({ vertexColors: true })), group).visible = false
 
   }, [])
 
--- packages/front/src/app/e/fractal-grid/main.tsx
+++ packages/front/src/app/e/fractal-grid/main.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { GTAOPass } from 'three/examples/jsm/postprocessing/GTAOPass.js'
+
 import { VertigoControls } from 'some-utils-three/camera/vertigo/controls'
+import { PassType } from 'some-utils-three/contexts/webgl'
 import { onTick } from 'some-utils-ts/ticker'
 
 import { leak } from '@/utils/leak'
@@ -8,7 +11,7 @@ import { leak } from '@/utils/leak'
 import { FractalGrid } from './fractal-grid'
 import { ThreeProvider, useThree } from './three-provider'
 
-function Controller() {
+function Settings() {
   useThree(function* (three) {
     leak({ three })
     const controls = new VertigoControls({
@@ -19,6 +22,18 @@ function Controller() {
       const { aspect, camera } = three
       controls.update(camera, aspect, tick.deltaTime)
     })
+
+    const aoPass = new GTAOPass(three.scene, three.camera)
+    aoPass.updatePdMaterial({
+      lumaPhi: 10,
+      depthPhi: 2,
+      normalPhi: 3,
+      radius: 1,
+      radiusExponent: 2,
+      rings: 2,
+      samples: 24,
+    })
+    yield three.pipeline.addPass(aoPass, { type: PassType.PostProcessing })
   }, [])
   return null
 }
@@ -28,7 +43,7 @@ export function Main() {
     <div className='FractalGrid'>
       <ThreeProvider>
         <h1>FractalGrid</h1>
-        <Controller />
+        <Settings />
         <FractalGrid />
       </ThreeProvider>
     </div>
