c37074ad1b98858f6c8acfd56fb87a475926abd0
jniac <joseph.merdrignac@gmail.com>
2025-09-05 18:24:03 +0200

    matrix with shear: the problem...

---
+++ packages/front/src/app/l/matrix-with-shear/0/ui.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { VertigoControls } from 'some-utils-three/camera/vertigo/controls'
+import { Message } from 'some-utils-ts/message'
+
+import s from './page.client.module.css'
+
+export function UI() {
+  return (
+    <div className={`${s.Client} ui layer thru p-8 flex flex-col gap-4 items-start`}>
+      <h1 className='text-2xl font-bold mb-8'>
+        Matrix with Shear
+      </h1>
+
+      <button
+        className='btn border rounded px-4 py-2'
+        onClick={() => {
+          const controls = Message.send<VertigoControls>(VertigoControls).assertPayload()
+          controls.vertigo.set({
+            perspective: 0,
+            zoom: 1,
+            rotation: [0, 0, 0],
+          })
+        }}
+      >
+        Orthographic (reset)
+      </button>
+
+      <button
+        className='btn border rounded px-4 py-2'
+        onClick={() => {
+          const controls = Message.send<VertigoControls>(VertigoControls).assertPayload()
+          controls.vertigo.set({
+            perspective: 1,
+          })
+        }}
+      >
+        Perspective
+      </button>
+
+      <p className='flex w-[400px]'>
+        The problem:<br />
+        Linear interpolation of matrix columns produces weird intermediate states.
+      </p>
+
+      <p className='flex w-[400px]'>
+        Conclusion:<br />
+        The matrices should be decomposed to position, rotation, scale, shear, interpolated, then recomposed.
+      </p>
+      <p className='flex w-[400px]'>
+        But shear is tricky...
+      </p>
+    </div>
+  )
+}
