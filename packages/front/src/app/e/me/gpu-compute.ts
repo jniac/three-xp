4867a3b381472af33ed8ede01852d79146e44d18
jniac <joseph.merdrignac@gmail.com>
2025-10-09 16:13:46 +0200

    gpu-compute & conway's game of life ok (ping pong ok)

--- packages/front/src/app/e/me/gpu-compute.ts
+++ packages/front/src/app/e/me/gpu-compute.ts
@@ -7,9 +7,9 @@ import { glsl_utils } from 'some-utils-ts/glsl/utils'
 function createGpuComputeMaterialUniforms() {
   return {
     uTexture: { value: <Texture | null>null },
+    uTextureSize: { value: new Vector2() },
     uTime: { value: 0 },
     uDeltaTime: { value: 0 },
-    uTextureSize: { value: new Vector2() },
   }
 }
 
@@ -40,6 +40,7 @@ class GpuComputeMaterial extends ShaderMaterial {
         ${glsl_utils}
         varying vec2 vUv;
         uniform sampler2D uTexture;
+        uniform vec2 uTextureSize;
         uniform float uTime;
         uniform float uDeltaTime;
         void main() {
@@ -58,9 +59,8 @@ const defaultParams = {
 export class GpuCompute {
   parts = {
     orthoCamera: new OrthographicCamera(-1, 1, 1, -1, 0, 1),
-    material: new GpuComputeMaterial(),
     plane: new Mesh(new PlaneGeometry(2, 2), undefined),
-  };
+  }
 
   params: typeof defaultParams
 
@@ -68,8 +68,17 @@ export class GpuCompute {
     size: Vector2
     rta: WebGLRenderTarget
     rtb: WebGLRenderTarget
+    time: number
     frame: number
   }
+
+  #innerState?: {
+    renderer: WebGLRenderer
+    initialMaterial: GpuComputeMaterial
+    updateMaterial: GpuComputeMaterial
+  }
+
+  get isInitialized() { return !!this.#innerState }
 
   constructor(userParams?: Partial<typeof defaultParams>) {
     const params = { ...defaultParams, ...userParams }
@@ -94,11 +103,16 @@ export class GpuCompute {
       size,
       rta,
       rtb,
+      time: 0,
       frame: 0,
     }
   }
 
-  initialize(renderer: WebGLRenderer, initialMaterialParams?: GpuComputeMaterialParams): this {
+  initialize(
+    renderer: WebGLRenderer,
+    initialMaterialParams?: GpuComputeMaterialParams,
+    updateMaterialParams?: GpuComputeMaterialParams,
+  ): this {
     const { orthoCamera, plane } = this.parts
     const { rta } = this.state
 
@@ -109,11 +123,40 @@ export class GpuCompute {
     initialMaterial.uniforms.uDeltaTime.value = 0
     plane.material = initialMaterial
 
+    const updateMaterial = new GpuComputeMaterial(updateMaterialParams)
+
+    this.#innerState = { renderer, initialMaterial, updateMaterial }
+
     renderer.setRenderTarget(rta)
     renderer.render(plane, orthoCamera)
     renderer.setRenderTarget(null)
 
     this.state.frame = 0
+
+    return this
+  }
+
+  update(deltaTime = 0): this {
+    if (!this.#innerState)
+      throw new Error('GpuCompute: not initialized')
+
+    const { renderer, updateMaterial } = this.#innerState
+    const { orthoCamera, plane } = this.parts
+    const { rta, rtb, time, frame } = this.state
+
+    this.state.time += deltaTime
+
+    updateMaterial.uniforms.uTextureSize.value.copy(this.state.size)
+    updateMaterial.uniforms.uTexture.value = frame % 2 === 0 ? rta.texture : rtb.texture
+    updateMaterial.uniforms.uTime.value = time
+    updateMaterial.uniforms.uDeltaTime.value = deltaTime
+    plane.material = updateMaterial
+
+    renderer.setRenderTarget(frame % 2 === 0 ? rtb : rta)
+    renderer.render(plane, orthoCamera)
+    renderer.setRenderTarget(null)
+
+    this.state.frame += 1
 
     return this
   }
--- packages/front/src/app/e/me/my-scene.ts
+++ packages/front/src/app/e/me/my-scene.ts
@@ -5,6 +5,7 @@ import { DebugTexture } from 'some-utils-three/textures/debug'
 import { setup } from 'some-utils-three/utils/tree'
 import { onTick } from 'some-utils-ts/ticker'
 
+import { handlePointer } from 'some-utils-dom/handle/pointer'
 import { GpuCompute } from './gpu-compute'
 
 export function MyScene() {
@@ -25,13 +26,63 @@ export function MyScene() {
         fragColor: /* glsl */`
           vec2 uv = vUv;
           float h = hash(uv);
-          h = step(0.1, h);
-          gl_FragColor = vec4(h, h, 1., 1.);
+          h = step(0.6, h);
+          gl_FragColor = vec4(h, h, 1.0, 1.0);
+        `,
+      }, {
+        fragColor: /* glsl */`
+          vec2 uv = vUv;
+          vec2 uv0 = uv + vec2(0.0, 1.0) / uTextureSize;
+          vec2 uv1 = uv + vec2(1.0, 1.0) / uTextureSize;
+          vec2 uv2 = uv + vec2(1.0, 0.0) / uTextureSize;
+          vec2 uv3 = uv + vec2(1.0, -1.0) / uTextureSize;
+          vec2 uv4 = uv + vec2(0.0, -1.0) / uTextureSize;
+          vec2 uv5 = uv + vec2(-1.0, -1.0) / uTextureSize;
+          vec2 uv6 = uv + vec2(-1.0, 0.0) / uTextureSize;
+          vec2 uv7 = uv + vec2(-1.0, 1.0) / uTextureSize;
+          float sum =
+            texture2D(uTexture, uv0).r +
+            texture2D(uTexture, uv1).r +
+            texture2D(uTexture, uv2).r +
+            texture2D(uTexture, uv3).r +
+            texture2D(uTexture, uv4).r +
+            texture2D(uTexture, uv5).r +
+            texture2D(uTexture, uv6).r +
+            texture2D(uTexture, uv7).r;
+
+          // Conway's Game of Life rules
+          float current = texture2D(uTexture, uv).r;
+          if (current > 0.5) {
+            if (sum < 1.5 || sum > 3.5) {
+              current = 0.0; // Cell dies
+            } else {
+              current = 1.0; // Cell lives
+            }
+          } else {
+            if (sum == 3.0) {
+              current = 1.0; // Cell becomes alive
+            }
+          }
+
+          gl_FragColor.rgb = vec3(current, current, 1.0);
         `,
       })
 
-    onTick('three', tick => {
+    let play = true
+
+    yield onTick('three', () => {
       plane.material.map = gpuCompute.currentTexture()
+    })
+
+    yield onTick('three', { timeInterval: .1 }, () => {
+      if (play)
+        gpuCompute.update()
+    })
+
+    yield handlePointer(document.body, {
+      onTap: () => {
+        play = !play
+      },
     })
 
   }, [])
