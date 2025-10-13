86a57d925bdbd6eb71fa15e74b031018a805b27d
jniac <joseph.merdrignac@gmail.com>
2025-10-13 16:16:56 +0200

    opengraph?

--- packages/front/src/app/e/me/page.tsx
+++ packages/front/src/app/e/me/page.tsx
@@ -4,6 +4,14 @@ import { ClientPage } from './page.client'
 
 export const metadata = new XpMetadata({
   slug: 'me',
+  title: 'Joseph M.',
+  openGraph: {
+    title: 'Joseph M. — Visual Tech Artist',
+    description: 'I am Joseph M., a Visual Tech Artist based in Paris, France. I create interactive 3D experiences on the web, blending art and technology to craft immersive digital worlds.',
+    images: [
+      '../assets/images/me.jpg'
+    ]
+  }
 })
 
 export default function FractalGridPage() {
