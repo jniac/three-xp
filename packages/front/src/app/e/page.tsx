3f076a35241046a42f091553fe6042cad70f078b
jniac <joseph.merdrignac@gmail.com>
2025-12-02 15:23:13 +0100

    (rfctr) "liwa-ui" -> "circle-packing"

--- packages/front/src/app/e/page.tsx
+++ packages/front/src/app/e/page.tsx
@@ -6,13 +6,13 @@ import { SummaryPage } from '@/components/summary'
 import * as aoTransparent from './ao-transparent/page'
 import * as art1 from './art-1/page'
 import * as art2 from './art-2/page'
+import * as circlePackingP0 from './circle-packing/p0/page'
 import * as depthPlay from './depth-play/page'
 import * as dotsAndFalloffs from './dots-and-falloffs/page'
 import * as fourCircles from './four-circles/page'
 import * as fractalGrid from './fractal-grid/page'
 import * as fromEarthTo from './from-earth-to/page'
 import * as ladyEthereal from './lady-ethereal/page'
-import * as liwaUiP0 from './liwa-ui/p0/page'
 import * as me from './me/page'
 import * as nineCircles from './nine-circles/page'
 import * as ovvoLayers from './ovvo/layers/page'
@@ -26,13 +26,13 @@ const pages = {
   'ao-transparent': aoTransparent,
   'art-1': art1,
   'art-2': art2,
+  'circle-packing/p0': circlePackingP0,
   'depth-play': depthPlay,
   'dots-and-falloffs': dotsAndFalloffs,
   'four-circles': fourCircles,
   'fractal-grid': fractalGrid,
   'from-earth-to': fromEarthTo,
   'lady-ethereal': ladyEthereal,
-  'liwa-ui/p0': liwaUiP0,
   'me': me,
   'nine-circles': nineCircles,
   'ovvo/layers': ovvoLayers,
